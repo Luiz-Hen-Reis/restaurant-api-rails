5a540787385eb1a3d51c622cec5eabbf523d8cae
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-17 19:47:14 -0300

    feat: create decode_token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, ENV['SECRET'])
   end
+
+  def decode_token
+    auth_header = request.headers['Authorization']
+
+    if auth_header
+      token = auth_header.spli(' ').last
+
+      begin
+        JWT.decode(token, ENV['SECRET'], true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
 end
