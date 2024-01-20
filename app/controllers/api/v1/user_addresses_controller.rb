ae82dc9be2b5fd356e48ea4d3a3f2a381371b0fa
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-20 19:52:15 -0300

    feat: add create method for posting new addresses

--- app/controllers/api/v1/user_addresses_controller.rb
+++ app/controllers/api/v1/user_addresses_controller.rb
@@ -1,7 +1,29 @@
 class Api::V1::UserAddressesController < ApplicationController
   before_action :authorize
+  before_action :set_user
 
   def index
-    @addresses = UserAddress.all
+    @addresses = UserAddress.where(user_id: @user.id)
+  end
+
+  def create
+    @address = UserAddress.new(address_params)
+    @address.user_id = @user.id
+
+    if @address.save
+      render json: @address, status: :created
+    else
+      render json: @address.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = authorized_user
+  end
+
+  def address_params
+    params.require(:user_address).permit(:street, :street_number, :zipcode, :district, :complement)
   end
 end
