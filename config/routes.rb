863a58e3fb2a152da4041148b453552a269ef161
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-20 12:38:14 -0300

    chore: create user_addresses route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
     post 'register', to: 'authentication#register'
     post 'login', to: 'authentication#login'
   end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :user_addresses, only: [:index]
+    end
+  end
 end
