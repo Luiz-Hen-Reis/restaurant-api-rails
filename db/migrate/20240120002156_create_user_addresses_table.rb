992531f8da9c6bfdac781f3642a33b5e0af4394c
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-20 12:37:23 -0300

    fix: fix user_addresses table name in migration file

--- db/migrate/20240120002156_create_user_addresses_table.rb
+++ db/migrate/20240120002156_create_user_addresses_table.rb
@@ -1,9 +1,9 @@
 class CreateUserAddressesTable < ActiveRecord::Migration[7.1]
   def change
-    create_table :user_addresses_tables, id: :uuid do |t|
+    create_table :user_addresses, id: :uuid do |t|
       t.references :user, type: :uuid, null: false, foreign_key: true
       t.string :street, null: false
-      t.string :StreetNumber, null: false
+      t.string :street_number, null: false
       t.string :zipcode, null: false
       t.string :complement, null: false
       t.string :district, null: false
