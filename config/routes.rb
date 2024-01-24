78de3be9a3ebecdfdc42f8595d13f88b6703b799
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-23 21:01:03 -0300

    chore: create products resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :user_addresses, only: [:index, :create]
       resources :categories, only: [:index, :show]
+      resources :products, only: [:index, :show]
     end
   end
 end
