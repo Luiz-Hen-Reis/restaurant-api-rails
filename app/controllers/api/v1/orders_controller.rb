2a5004fe512ac5573779f53954ca4fb7ed2f80c5
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-25 20:59:03 -0300

    feat: create orders controller with index and create actions

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::OrdersController < ApplicationController
+  before_action :authorize
+  before_action :set_user
+
+  def index
+    @orders = Order.where(user_id: @user.id)
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = @user.id
+
+    if @order.create
+      render json: @order, status: :created
+    else
+      render json: @order.errors, status: :unprocessable_entity
+  end
+
+  private
+
+  def order_params
+    params.require(:orders).permit(:payment_method, :payment_cash_return,
+    :delivery_price, :subtotal, :order_date)
+  end
+end
