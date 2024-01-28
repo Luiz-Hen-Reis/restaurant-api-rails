65f8961a0ef8b0f4daab61300476f8b5be0f5549
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-28 13:09:46 -0300

    chore: create set_price e set_delivery_price to calculate total_value of the orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,23 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :order_products
 
   validates_associated :order_products
+
+  before_validation :set_delivery_price
+  before_validation :set_price
+
+  private
+
+  def set_delivery_price
+    self.delivery_price = 3.50
+  end
+
+  def set_price
+    final_price = 0
+
+    order_products.each do |op|
+      final_price += op.quantity * op.product.price
+    end
+
+    self.total_price = final_price + self.delivery_price
+  end
 end
