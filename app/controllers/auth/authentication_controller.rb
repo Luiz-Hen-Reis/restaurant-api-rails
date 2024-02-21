43db8e8b8b6efbfc6a87e2f737a31f5507261a62
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-20 21:07:39 -0300

    feat: create show action to fetch and return user

--- app/controllers/auth/authentication_controller.rb
+++ app/controllers/auth/authentication_controller.rb
@@ -28,6 +28,18 @@ module Auth
       end
     end
 
+    def show
+      @user = User.find_by(id: params[:id])
+
+      if @user.present?
+        render 'show',
+        status: :ok
+      else
+        render json: { error: "Usuário não existe." },
+        status: :unauthorized
+      end
+    end
+
     private
 
     def user_params
